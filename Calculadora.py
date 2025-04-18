f71ff5470c30b99faad75ac4f25c50e17f02ef0d
Joaquín Diaz <119331530+crisjoacko@users.noreply.github.com>
2025-04-18 12:42:38 -0300

    Mejorando la calculadora

--- Calculadora.py
+++ Calculadora.py
@@ -1,16 +1,32 @@
-num1=float(input ("primerNumero"))
-operacion=input("operacion (+,-,/,%)")
-num2=float(input("segundoNumero"))
-if operacion == '+':
-    print (num1+num2)
-elif operacion == '-':
-    print (num1-num2)
-elif operacion == '%':
-    print (num1%num2)
-elif operacion == '/':
-    if num2 !=0:
-        print(num1/num2)
+while True:
+    print ("Calculadora")
+    print ("1.Suma")
+    print ("2.Resta")
+    print ("3.Multiplicacion")
+    print ("4.Division")
+    print ("5.Potencia")
+    print ("6.Salir")
+    
+    opcion = input ("Elige una opcion (1-6):")
+    
+    if opcion == "6":
+        print ("Hasta la proxima")
+        break
+    
+    num1=float(input ("primerNumero"))
+    num2=float(input("segundoNumero"))
+    if opcion == "1":
+        print (f"Resultado: {num1+num2}")
+    elif opcion == "2":
+        print (f"Resultado: {num1-num2}")
+    elif opcion == "3":
+        print (f"Resultado:{num1*num2}")
+    elif opcion == "4":
+        if num2 !=0:
+            print (f"Resultado:{num1/num2}")
+        else:
+            print ("No se puede dividir por cero.")
+    elif opcion == "5":
+        print (f"Resultado{num1**num2}")
     else:
-        print ("No se puede dividir por 0")
-else:
-    print ("Operacion no valida")
+        print("Opcion no valida, por favor elige una opcion entre 1 y 6")
